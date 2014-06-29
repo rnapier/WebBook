4d1397ded24b4bff63791f614130629dbb4ec3cb
Rob Napier <robnapier@gmail.com>
2014-06-29 08:43:29 -0400

    Initial Commit

---
+++ WebBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebBook
+//
+//  Created by Rob Napier on 6/29/14.
+//  Copyright (c) 2014 Rob Napier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
