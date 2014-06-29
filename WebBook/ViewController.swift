ab32e120bed619e1f6b86cb61a04809c8a94662d
Rob Napier <robnapier@gmail.com>
2014-06-29 13:50:31 -0400

    custom embed with animation

--- WebBook/ViewController.swift
+++ WebBook/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import QuartzCore
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
                             
   @IBOutlet var webView: UIWebView
   
@@ -17,7 +18,7 @@ class ViewController: UIViewController {
     webView.scrollView.pagingEnabled = true
     webView.scrollView.alwaysBounceHorizontal = true
     webView.scrollView.alwaysBounceVertical = false
-    
+    webView.delegate = self
 
     let path = NSBundle.mainBundle().pathForResource("Simple", ofType: "html")
     let htmlData = NSData(contentsOfFile:path)
@@ -32,5 +33,31 @@ class ViewController: UIViewController {
   }
 
 
+  func rectForElement(elementID: String) -> CGRect {
+    let js = "function f(){ var r = document.getElementById('%@').getBoundingClientRect(); return '{{'+r.left+','+r.top+'},{'+r.width+','+r.height+'}}'; } f();";
+    let result = webView.stringByEvaluatingJavaScriptFromString(String(format:js, elementID))
+    let rect = CGRectFromString(result)
+    return rect;
+  }
+
+  func webViewDidFinishLoad(webView: UIWebView!) {
+    let r = rectForElement("embed1")
+    let v = UIView(frame:r)
+    v.backgroundColor = UIColor.redColor()
+    let b = UIButton.buttonWithType(.ContactAdd) as UIButton
+    b.frame = CGRectMake(50, 50, 100, 100)
+    b.backgroundColor = UIColor.blueColor()
+    v.addSubview(b)
+
+    let anim = CABasicAnimation(keyPath: "transform.rotation.z")
+    anim.fromValue = 0
+    anim.toValue = 2 * M_PI
+    anim.repeatCount = HUGE
+    anim.duration = 5
+    b.layer.addAnimation(anim, forKey: "rotate")
+
+
+    webView.scrollView.addSubview(v)
+  }
 }
 
