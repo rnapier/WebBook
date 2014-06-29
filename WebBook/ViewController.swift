e3880567515eea7ceea151ce36dc5810159d3477
Rob Napier <robnapier@gmail.com>
2014-06-29 08:50:31 -0400

    Just show it in a webview

--- WebBook/ViewController.swift
+++ WebBook/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+  @IBOutlet var webView: UIWebView
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    let path = NSBundle.mainBundle().pathForResource("Book", ofType: "html")
+    let htmlData = NSData(contentsOfFile:path)
+    webView.loadData(htmlData, MIMEType: nil, textEncodingName: nil, baseURL: nil)
+
     // Do any additional setup after loading the view, typically from a nib.
   }
 
