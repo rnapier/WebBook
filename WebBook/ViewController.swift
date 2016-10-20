83962535198f4f04ec5c905d6d35a9c82043476d
Rob Napier <robnapier@gmail.com>
2016-10-20 12:13:59 -0400

    Upgrade to Swift 3.

--- WebBook/ViewController.swift
+++ WebBook/ViewController.swift
@@ -14,48 +14,41 @@ class ViewController: UIViewController, UIWebViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.webView?.scrollView.pagingEnabled = true
+        self.webView?.scrollView.isPagingEnabled = true
         self.webView?.scrollView.alwaysBounceHorizontal = true
         self.webView?.scrollView.alwaysBounceVertical = false
         self.webView?.delegate = self
 
-        if let path = NSBundle.mainBundle().pathForResource("Simple", ofType: "html") {
-            let htmlData = NSData(contentsOfFile:path)
-            self.webView?.loadData(htmlData, MIMEType: nil, textEncodingName: nil, baseURL: nil)
+        if let path = Bundle.main.path(forResource: "Simple", ofType: "html") {
+            let url = URL(fileURLWithPath: path)
+            let htmlData = try! Data(contentsOf: url)
+            self.webView?.load(htmlData, mimeType: "text/html", textEncodingName: "utf-8", baseURL: url)
         }
     }
 
-    func rectForElement(elementID: String) -> CGRect {
-        // This and other functions could of course be put inside the HTML, 
-        // which would make writing and testing them simpler. But this
-        // shows how it could work even on existing HTML.
-        let js = "function f(){ var r = document.getElementById('%@').getBoundingClientRect(); return '{{'+r.left+','+r.top+'},{'+r.width+','+r.height+'}}'; } f();";
-        let result = webView?.stringByEvaluatingJavaScriptFromString(String(format:js, elementID))
-        let rect = CGRectFromString(result)
-        return rect;
-    }
-
-    func webViewDidFinishLoad(webView: UIWebView) {
+    // =====
+    func webViewDidFinishLoad(_ webView: UIWebView) {
         self.insertCustomView(webView)
     }
 
     // Whatever you want. This is just a silly toy that you can drag
     // a rotating box around. The point is that it's a totally arbitrary
     // view with its own gesture recognizers, buttons, etc.
-    func insertCustomView(webView: UIWebView) {
+    func insertCustomView(_ webView: UIWebView) {
         let r = rectForElement("embed1")
+
         let v = UIView(frame:r)
-        v.backgroundColor = UIColor.redColor()
-        let b = UIButton.buttonWithType(.ContactAdd) as! UIButton
-        b.frame = CGRectMake(50, 50, 100, 100)
-        b.backgroundColor = UIColor.blueColor()
-        b.addTarget(self, action: Selector("click:"), forControlEvents: .TouchUpInside)
+        v.backgroundColor = UIColor.red
+        let b = UIButton(type: .contactAdd)
+        b.frame = CGRect(x: 50, y: 50, width: 100, height: 100)
+        b.backgroundColor = UIColor.blue
+        b.addTarget(self, action: #selector(ViewController.click(_:)), for: .touchUpInside)
         v.addSubview(b)
 
         let l = UILabel()
         l.text = "I'm a UIView. Drag the box."
         l.sizeToFit()
-        l.center = CGPointMake(CGRectGetMidX(v.bounds), CGRectGetMidY(v.bounds))
+        l.center = CGPoint(x: v.bounds.midX, y: v.bounds.midY)
         v.addSubview(l)
 
         let anim = CABasicAnimation(keyPath: "transform.rotation.z")
@@ -63,37 +56,47 @@ class ViewController: UIViewController, UIWebViewDelegate {
         anim.toValue = 2 * M_PI
         anim.repeatCount = HUGE
         anim.duration = 5
-        b.layer.addAnimation(anim, forKey: "rotate")
+        b.layer.add(anim, forKey: "rotate")
 
-        let g = UIPanGestureRecognizer(target: self, action: Selector("drag:"))
+        let g = UIPanGestureRecognizer(target: self, action: #selector(ViewController.drag(_:)))
         b.addGestureRecognizer(g)
 
         webView.scrollView.addSubview(v)
     }
 
+    func rectForElement(_ elementID: String) -> CGRect {
+        // This and other functions could of course be put inside the HTML,
+        // which would make writing and testing them simpler. But this
+        // shows how it could work even on existing HTML.
+        let js = "function f(){ var r = document.getElementById('%@').getBoundingClientRect(); return '{{'+r.left+','+r.top+'},{'+r.width+','+r.height+'}}'; } f();";
+        let result = webView?.stringByEvaluatingJavaScript(from: String(format:js, elementID))
+        let rect = CGRectFromString(result!)
+        return rect;
+    }
+    // =====
 
     // Silly helper for the custom view
-    func drag(g: UIPanGestureRecognizer) {
-        func boundBy(x: CGFloat, min: CGFloat, max: CGFloat) -> CGFloat {
+    func drag(_ g: UIPanGestureRecognizer) {
+        func boundBy(_ x: CGFloat, min: CGFloat, max: CGFloat) -> CGFloat {
             if x > max { return max }
             if x < min { return min }
             return x
         }
 
-        let dragLocation = g.locationInView(g.view?.superview)
-        let box = g.view?.superview?.bounds ?? CGRectZero
-        let finalLocation = CGPointMake(
-            boundBy(dragLocation.x, CGRectGetMinX(box), CGRectGetMaxX(box)),
-            boundBy(dragLocation.y, CGRectGetMinY(box), CGRectGetMaxY(box)))
+        let dragLocation = g.location(in: g.view?.superview)
+        let box = g.view?.superview?.bounds ?? CGRect.zero
+        let finalLocation = CGPoint(
+            x: boundBy(dragLocation.x, min: box.minX, max: box.maxX),
+            y: boundBy(dragLocation.y, min: box.minY, max: box.maxY))
         g.view?.center = finalLocation
     }
 
     // And we can handle click events too. What more could you want?
-    func click(b: UIButton) {
-        if b.backgroundColor == UIColor.blueColor() {
-            b.backgroundColor = UIColor.greenColor()
+    func click(_ b: UIButton) {
+        if b.backgroundColor == UIColor.blue {
+            b.backgroundColor = UIColor.green
         } else {
-            b.backgroundColor = UIColor.blueColor()
+            b.backgroundColor = UIColor.blue
         }
     }
 }
